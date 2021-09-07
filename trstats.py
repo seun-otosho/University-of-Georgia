b9c36ebb713de23872eef4e4c4e9e54953d1ae95
oomotosho <toloruntotosho@gmail.com>
2021-09-07 11:12:28 +0100

    Computation done and working

--- trstats.py
+++ trstats.py
@@ -16,68 +16,45 @@ import pandas as pd
 
 from logger import get_logger
 
-out_dir = str(Path(__file__).resolve().parent / 'output')
+logger, prev_hop_list = get_logger(), []
+# check_stamp = None
+out_dir = str(Path(__file__).resolve().parent / 'traces to')
 
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-n', dest='NUM_RUNS', default=1, metavar='NUM_RUNS', type=int, help='Number of times traceroute will run'
-)
-parser.add_argument(
-    '-d', dest='RUN_DELAY', default=None, metavar='RUN_DELAY', type=float,
-    help='Number of seconds to wait between two consecutive runs')
-parser.add_argument(
-    '-m', dest='MAX_HOPS', default=52, metavar='MAX_HOPS', type=int, help='Number of times traceroute will run')
-parser.add_argument(
-    '-o', dest='OUTPUT', default=os.getcwd() + os.sep + 'output.txt', metavar='OUTPUT', type=str,
-    help='Path and name of output JSON file containing the stats')
-parser.add_argument(
-    '-g', dest='GRAPH', default=None, metavar='GRAPH', type=str,
-    help='Path and name of output PDF file containing stats graph')
-parser.add_argument(
-    '-t', dest='TARGET', default=None, metavar='TARGET', type=str, help='A target domain name or IP address')
-parser.add_argument(
-    '--test', dest='TEST_DIR', default=os.getcwd() + '/folder/output.txt', metavar='TEST_DIR', type=str,
-    help='Directory containing num_runs text files, each of which contains the output of a traceroute run.If present, '
-         'this will override all other options and tcpdump will not be invoked. Stats will be computed over the '
-         'traceroute output stored in the text files')
-args = parser.parse_args()
 
-output, prev_hop_list, tr_dict_list, tr_output, trlist = {}, None, [], None, []
-
-
-# logger = get_logger()
-
-
-def check_file(ifile):
+def check_file(ifile, check_stamp):
     global logger
     try:
         if os.path.exists(ifile):
             file, ext = os.path.splitext(ifile)
-            nfile = file + str(datetime.now())[:-7].replace(':', '') + ext
+            nfile = f"{file} {check_stamp}{ext}"
             os.rename(ifile, nfile)
     except Exception as e:
         logger.error(e)
 
 
-def traceroute_to_file(ofile):
+def get_stamp():
+    return str(datetime.now())[:-7].replace(':', '')
+
+
+def traceroute_to_file(data, ofile, msg=None):
     global logger
     with open(ofile, 'a+') as outfile:
         outfile.write(f"{msg}\n")
-        outfile.write(tr_output)
+        outfile.write(data)
         outfile.write(f"\n{'=' * 88}\n\n")
 
 
 def traceroute_to_json(jfile, tr_dict_list=None):
-    global logger
-    check_file(jfile)
+    global logger, check_stamp
+    check_file(jfile, check_stamp)
     with open(jfile, 'w', encoding='utf-8') as outfile:
         json.dump(tr_dict_list, outfile, indent=4)
 
 
-def check_delay():
+def check_delay(i, args):
     global logger
     if i < args.NUM_RUNS:
         if args.RUN_DELAY:
@@ -90,7 +67,7 @@ def trace_hops(trlist: list):
     global logger, prev_hop_list
     for i, line in enumerate(trlist):
         if 'traceroute' not in line:
-            hop_list = [i for i in line.strip().split('  ')]
+            hop_list = [i for i in line.replace('*', '').strip().split('  ')]
             if i == 0 or i == len(trlist) - 1:
                 # tr_listings.append(hop_list)
                 yield hop_list
@@ -107,104 +84,119 @@ def trace_hops(trlist: list):
                     prev_hop_list = hop_list
 
 
-def process_tr_output():
+def process_tr_output(output):
     global logger
+    tr_dict_list = []
     for k, tr_output in output.items():
         trlist = tr_output.splitlines()
-        tr_listings = list(trh for trh in trace_hops(trlist) if trh and len(trh) > 0 and all('*' not in i for i in trh))
+        tr_listings = list(trh for trh in trace_hops(trlist) if trh and len(trh) > 0)
         for line in tr_listings:
             logger.info(f"{line = }")
             hop_dict = {}
+
             speeds = [float(i.split(' ')[0]) for i in line if i.endswith('ms')]
-            sorted(speeds)
-            hop_dict['avg'] = round(mean(speeds), 3)
             logger.info(f"{speeds = }")
-            hop_dict['hop'] = line[0]
+            hop_dict['hop'], hop_dict['speeds'] = line[0], speeds
             hop_dict['hosts'] = [i for i in line if not i.isdigit() and not i.endswith('ms')]
-            hop_dict['max'] = max(speeds)
-            hop_dict['med'] = median(speeds)
-            hop_dict['min'] = min(speeds)
-            # logger.info(stat)
+
             tr_dict_list.append(hop_dict)
             logger.info('=' * 88)
             logger.info(hop_dict)
+
     return tr_dict_list
 
 
 def multi_tr_pro(multi_tr_list, file):
-    global logger
+    global check_stamp, logger
     combined_dict = {}
     for i, d in enumerate(multi_tr_list):
         d_i = d['hop']
         if d_i in combined_dict:
             for k, v in d.items():
-                if k not in ('hop', 'hosts',):
-                    combined_dict[d_i][k] = combined_dict[d_i][k].append(v)
-                if k == 'hosts':
+                if k in ('hosts', 'speeds', ):
                     combined_dict[d_i][k] = combined_dict[d_i][k] + v
         else:
-            combined_dict[d_i] = {k: [v, ] for k, v in d.items() if k not in ('hop', 'hosts',)}
-            combined_dict[d_i]['hop'], combined_dict[d_i]['hosts'] = d['hop'], d['hosts']
+            combined_dict[d_i] = {}
+            combined_dict[d_i]['hop'] = d['hop']
+            combined_dict[d_i]['hosts'] = d['hosts']
+            combined_dict[d_i]['speeds'] = d['speeds']
 
-    check_file(file)
+    check_file(file, check_stamp)
     traceroute_to_json(file, list(combined_dict.values()))
+    return combined_dict
 
 
-if args.TARGET:
-    global logger
-    logger = get_logger(f"trace to {args.TARGET}")
-    logger.info(args)
-    jfile, jfyl_re, tfile = f'trace to {args.TARGET} data.json', f'trace to {args.TARGET} re.json', f'trace to {args.TARGET}.txt'
-    # if args.NUM_RUNS:# and args.NUM_RUNS > 1:
-    check_file(tfile)
-    multi_tr_list = []
-    for i in range(1, args.NUM_RUNS + 1):
-        # f.write('================================================1 \n')
-        msg = f"Running Traceroute to {args.TARGET}"
-        logger.info(msg)
-        rslt = subprocess.run(
-            ['traceroute', "-m", str(args.MAX_HOPS), args.TARGET], stdout=subprocess.PIPE  # , check=False
-        )
-        tr_output = rslt.stdout.decode('utf-8')
-        # write trace output to file
-        traceroute_to_file(tfile)
-        msg = f"Traceroute {i} completed"
-        logger.info(msg)
-        logger.info(tr_output)
-        output[i] = tr_output
+def main(argv=None):
+    global logger, prev_hop_list
+    logger.info(f"{argv = }")
+    output ={}
+    logger.info(f"{sys.argv = }")
+    args = parse_trace_args()
+    if args.TARGET:
+        # global logger
+        global check_stamp
+        check_stamp = get_stamp()
+        logger = get_logger(f"trace to {args.TARGET}")
+        logger.info(args)
+        jfile, jfyl_re = f'{out_dir}{os.sep}{args.TARGET} data.json', f'{out_dir}{os.sep}{args.TARGET} re.json'
+        cfile, tfile = f'{out_dir}{os.sep}{args.TARGET} cmptd.json', f'{out_dir}{os.sep}{args.TARGET}.txt'
+        check_file(tfile, check_stamp)
+        for i in range(1, args.NUM_RUNS + 1):
+            msg = f"Running Traceroute to {args.TARGET}"
+            logger.info(msg)
+            rslt = subprocess.run(
+                ['traceroute', "-m", str(args.MAX_HOPS), args.TARGET], stdout=subprocess.PIPE  # , check=False
+            )
+            tr_output = rslt.stdout.decode('utf-8')
+            # write trace output to file
+            traceroute_to_file(tr_output, tfile, msg)
+            msg = f"Traceroute {i} completed"
+            logger.info(msg)
+            logger.info(tr_output)
+            output[i] = tr_output
 
-        check_delay()
+            check_delay(i, args)
 
-    multi_tr_list = process_tr_output()
-    # process array/list of dicts
-    multi_tr_pro(multi_tr_list, jfyl_re)
-    tr_json = json.dumps(multi_tr_list, indent=4)
+        multi_tr_list = process_tr_output(output)
+        # process array/list of dicts
+        d4c = multi_tr_pro(multi_tr_list, jfyl_re)
+        computed = [{
+            "hop": d["hop"], "hosts": d["hosts"],
+            "avg": round(mean(d["speeds"]), 3), "min": min(d["speeds"]), "med": round(median(sorted(d["speeds"])), 3),
+            "max": max(d["speeds"]),
+        } for i, d in d4c.items() if len(d["speeds"])>0]
+        logger.info('=' * 88)
+        traceroute_to_json(jfile, multi_tr_list)
+        check_file(cfile, check_stamp)
+        traceroute_to_json(cfile, computed)
 
-    logger.info(tr_json)
-    logger.info('=' * 88)
-    # check if file exist rename and add datetime stamp to backup
-    traceroute_to_json(jfile)
 
-    # df=pd.DataFrame.from_dict(tr_json, orient='columns')
-    ##df.to_csv(jfile, header=None, index=None, sep=' ', mode='a')
-    # logger.info(df.head())
-    # sns.boxplot(y=df["avg"], x=df["hop"])
-    # plt.show()
+def parse_trace_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-n', dest='NUM_RUNS', default=1, metavar='NUM_RUNS', type=int, help='Number of times traceroute will run'
+    )
+    parser.add_argument(
+        '-d', dest='RUN_DELAY', default=None, metavar='RUN_DELAY', type=float,
+        help='Number of seconds to wait between two consecutive runs')
+    parser.add_argument(
+        '-m', dest='MAX_HOPS', default=52, metavar='MAX_HOPS', type=int, help='Number of times traceroute will run')
+    parser.add_argument(
+        '-o', dest='OUTPUT', default=os.getcwd() + os.sep + 'output.txt', metavar='OUTPUT', type=str,
+        help='Path and name of output JSON file containing the stats')
+    parser.add_argument(
+        '-g', dest='GRAPH', default=None, metavar='GRAPH', type=str,
+        help='Path and name of output PDF file containing stats graph')
+    parser.add_argument(
+        '-t', dest='TARGET', default=None, metavar='TARGET', type=str, help='A target domain name or IP address')
+    parser.add_argument(
+        '--test', dest='TEST_DIR', default=os.getcwd() + '/folder/output.txt', metavar='TEST_DIR', type=str,
+        help='Directory containing num_runs text files, each of which contains the output of a traceroute run. '
+             'If present, this will override all other options and tcpdump will not be invoked. '
+             'Stats will be computed over the  traceroute output stored in the text files')
+    args = parser.parse_args()
+    return args
 
-    # else:
-    #    msg = f"Running Traceroute to {args.TARGET}"
-    #    rslt = subprocess.run(
-    #        ['traceroute', "-m", str(args.MAX_HOPS), args.TARGET], stdout=subprocess.PIPE  # , check=False
-    #    )
-    #    tr_output = rslt.stdout.decode('utf-8')
-    #    msg = f"Traceroute completed"
-    #    logger.info(msg)
-    #    # write trace output to file
-    #    traceroute_to_file(tfile)
-    #    logger.info(tr_output)
-    #    output['0'] = tr_output
-#
-#    tr_dict_list = process_tr_output()
-#    tr_json = json.dumps(tr_dict_list)
-#    logger.info(tr_json)
-#    traceroute_to_json(jfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
