7ee532f2895980b6778a816a8d8c4c0a0dd9627f
oomotosho <toloruntotosho@gmail.com>
2021-09-10 06:57:35 +0100

    REwork3D & Working. ..

--- trstats.py
+++ trstats.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import re
 import subprocess
 import sys
 import time
@@ -10,7 +11,6 @@ from pathlib import Path
 from statistics import mean, median
 
 import plotly.express as px
-import plotly.io as pio
 
 import pandas as pd
 
@@ -18,8 +18,9 @@ import pandas as pd
 from logger import get_logger
 
 logger, prev_hop_list = get_logger(), []
+SPEED_PATTERN = '(\d*\.\d+|\d+) ms'
 # check_stamp = None
-out_dir = str(Path(__file__).resolve().parent / 'traces to')
+out_dir = str(Path(__file__).resolve().parent / 'traces')
 
 
 def check_tr_path(out_dir):
@@ -30,12 +31,14 @@ def check_tr_path(out_dir):
 check_tr_path(out_dir)
 
 
-def check_file(ifile, check_stamp):
-    global logger
+def check_file(ifile):
+    global logger, check_stamp
     try:
         if os.path.exists(ifile):
-            file, ext = os.path.splitext(ifile)
-            nfile = f"{file} {check_stamp}{ext}"
+            dirs, file = os.path.split(ifile)
+            ndir = f"{dirs}{os.sep}{check_stamp}"
+            check_tr_path(ndir)
+            nfile = f"{ndir}{os.sep}{file}"
             os.rename(ifile, nfile)
     except Exception as e:
         logger.error(e)
@@ -55,7 +58,7 @@ def traceroute_to_file(data, ofile, msg=None):
 
 def traceroute_to_json(jfile, tr_dict_list=None):
     global logger, check_stamp
-    check_file(jfile, check_stamp)
+    check_file(jfile)
     with open(jfile, 'w', encoding='utf-8') as outfile:
         json.dump(tr_dict_list, outfile, indent=4)
 
@@ -76,17 +79,17 @@ def trace_hops(trlist: list):
             hop_list = [i for i in line.replace('*', '').strip().split('  ')]
             if i == 0 or i == len(trlist) - 1:
                 # tr_listings.append(hop_list)
-                yield hop_list
+                yield '  '.join(hop_list)
             else:
                 if not hop_list[0].isdigit():
                     if prev_hop_list[0].isdigit() and hop_list[0].isdigit():
                         # tr_listings.append(prev_hop_list)
-                        yield prev_hop_list
+                        yield '  '.join(prev_hop_list)
                     else:
                         prev_hop_list += hop_list
                 else:
                     # tr_listings.append(prev_hop_list)
-                    yield prev_hop_list
+                    yield '  '.join(prev_hop_list)
                     prev_hop_list = hop_list
 
 
@@ -99,11 +102,12 @@ def process_tr_output(output):
         for line in tr_listings:
             logger.info(f"{line = }")
             hop_dict = {}
-
-            speeds = [float(i.split(' ')[0]) for i in line if i.endswith('ms')]
+            # speeds = [float(i.split(' ')[0]) for i in line if i.endswith('ms')]
+            speeds = re.findall(SPEED_PATTERN, line)
+            speeds = [float(s) for s in speeds]
+            line = re.sub(SPEED_PATTERN, '', line).strip()
             logger.info(f"{speeds = }")
-            hop_dict['hop'], hop_dict['speeds'] = line[0], speeds
-            hop_dict['hosts'] = [i for i in line if not i.isdigit() and not i.endswith('ms')]
+            hop_dict['hop'], hop_dict['hosts'], hop_dict['speeds'] = line.split('  ')[0], line.split('  ')[1:], speeds
 
             tr_dict_list.append(hop_dict)
             logger.info('=' * 88)
@@ -127,7 +131,7 @@ def multi_tr_pro(multi_tr_list, file):
             combined_dict[d_i]['hosts'] = d['hosts']
             combined_dict[d_i]['speeds'] = d['speeds']
 
-    check_file(file, check_stamp)
+    check_file(file)
     traceroute_to_json(file, list(combined_dict.values()))
     return combined_dict
 
@@ -143,13 +147,15 @@ def main(argv=None):
         global check_stamp
         check_stamp = get_stamp()
         logger = get_logger(f"trace to {args.TARGET}")
+        out_dir2u = f"{out_dir}{os.sep}to {args.TARGET}"
+        check_tr_path(out_dir2u)
         logger.info(args)
-        cfile = f'{out_dir}{os.sep}{args.TARGET} cmptd.json'
+        cfile = f'{out_dir2u}{os.sep}{args.TARGET} cmptd.json'
+        jdata_file = f'{out_dir2u}{os.sep}{args.TARGET} data.json'
+        jdata_file_re = f'{out_dir2u}{os.sep}{args.TARGET} re.json'
         for i in range(1, args.NUM_RUNS + 1):
-            tfile = f'{out_dir}{os.sep}{args.TARGET}-{i}.txt'
-            jfile = f'{out_dir}{os.sep}{args.TARGET}-{i} data.json'
-            jfyl_re = f'{out_dir}{os.sep}{args.TARGET}-{i} re.json'
-            check_file(tfile, check_stamp)
+            tfile = f'{out_dir2u}{os.sep}{args.TARGET}-{i}.txt'
+            check_file(tfile)
             msg = f"Running Traceroute to {args.TARGET}"
             logger.info(msg)
             rslt = subprocess.run(
@@ -166,22 +172,29 @@ def main(argv=None):
             check_delay(i, args)
 
         multi_tr_list = process_tr_output(output)
-        traceroute_to_json(jfile, multi_tr_list)
+        traceroute_to_json(jdata_file_re, multi_tr_list)
 
-        d4c = multi_tr_pro(multi_tr_list, jfyl_re)
+        d4c = multi_tr_pro(multi_tr_list, jdata_file)
+        d4c = {i[0]: i[1] for i in sorted(d4c.items(), key=lambda x: int(x[0]))}
         computed = [{
-            "hop": d["hop"], "hosts": d["hosts"],
+            "hop": d["hop"], "hosts": list(set([h.strip() for h in d["hosts"] if h != ''])),
             "avg": round(mean(d["speeds"]), 3), "min": min(d["speeds"]), "med": round(median(sorted(d["speeds"])), 3),
             "max": max(d["speeds"]),
         } for i, d in d4c.items() if len(d["speeds"])>0]
         logger.info('=' * 88)
-        check_file(cfile, check_stamp)
+        check_file(cfile)
         traceroute_to_json(cfile, computed)
 
         dl = [{k: v for k, v in i.items() if k in ('hop', 'speeds',)} for i in d4c.values()]
         df = pd.DataFrame(dl)
 
         df = df.assign(speeds=df.speeds).explode('speeds')
+        if args.GRAPH:
+            fig = px.box(df, y=df["speeds"], x=df["hop"])
+            g_file = f'{out_dir2u}{os.sep}{args.GRAPH}.pdf'
+            check_file(g_file)
+            fig.write_image(g_file)
+            fig.show()
 
         return d4c
 
